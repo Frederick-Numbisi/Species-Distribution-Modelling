3947523fdb729b5f7b3726ee75803c312f4cea35
Frederick N. Numbisi <38797347+Frederick-Numbisi@users.noreply.github.com>
2025-02-28 22:34:23 +0100

    Update PMBSRSpeciesMapUpdate2.R

--- PMBSRSpeciesMapUpdate2.R
+++ PMBSRSpeciesMapUpdate2.R
@@ -1,36 +1,13 @@
-#library(metafor)
-
-# Install packages for creating Apps using R Studio Shiny
-#install.packages(c("gapminder", "ggforce", "gh", "globals", "openintro", "profvis",
-#                   "RDQLite", "shinycssloader", "shinyFeedback", "shinythemes", "testthat",
-#                   "thematic", "vroom", "waiter", "xml2", "zeallot"))
-
 
 library(basemaps) # to load basemaps
-#get_maptypes()
-#remove.packages('terra')
 library(leaflet)
-#install.packages('dismo')
-#install.packages('terra')
 library(dismo) # also loads the sp and raster packages
 library(dplyr)
 library(ggplot2)
 library(tidyverse)
 library(raster)
 library(sp)
-##library(googlesheets4)  # for reading googlesheets data
-##library(lubridate)  # for data manipulations
-#install.packages('map')
-##library(maps)  # for map data
-##library(ggmap)  # for mapping points on maps
-##library(ggthemes) # for more themes (including theme_map())
-#install.packages('tidyverse')
-#install.packages('googlesheets4')  # for reading googlesheet data
-#install.packages('lubridate')  # for data manipulations
-#install.packages('maps')  # for map data
-#install.packages('ggmap')  # for mapping points on maps
-#install.packages('ggthemes') # for more themes (including theme_map())
-#gs4_deauth()
+
 theme_set(theme_minimal())
 
 library(rgdal)
@@ -38,11 +15,11 @@ library(sf)
 library(maptools) # to add north arrow
 library(leaflet.extras)
 
-#setwd("/home/numbisi/Documents/JOB_Searches/RoyarlBotanicGardenEdinburg/RBGE_MajorFloras_CMEP/SpeciesDistributionMaps/Wadi_Ashar/")
-setwd("/home/numbisi/Documents/JOB_Searches/RoyarlBotanicGardenEdinburg/RBGE_MajorFloras_CMEP/PMBSR_Project/")
 
-filepath1 = '/home/numbisi/Documents/JOB_Searches/RoyarlBotanicGardenEdinburg/RBGE_MajorFloras_CMEP/PMBSR_Project/'
-filepath2 = '/home/numbisi/Documents/JOB_Searches/RoyarlBotanicGardenEdinburg/RBGE_MajorFloras_CMEP/PMBSR_Project/PMBSR_SpeciesMaps_Update/'
+setwd("/home/numbisi/Documents/pathToWorkingDirectory")
+
+filepath1 = '/home/numbisi/Documents/filePath1'
+filepath2 = '/home/numbisi/Documents/filePath2'
 
 
 # Create function to exclude row with duplicate coordinates
@@ -67,8 +44,6 @@ Occurrence2 = function(SpeciesData){
   return(SpeciesData)
 }
 
-
-
 # Create function to convert CRS from Degree Minute Seconds (DMS) to Decimal Degrees 
 Degree2dec <- function(angle) {
   angle <- as.character(angle)
@@ -90,8 +65,6 @@ Degree2dec <- function(angle) {
 
 
 psbrsShape = readOGR("./PMBSR/PMBSRDA.shp") # import shapefile of project extent
-
-#PMBSRSpeciesUpdate = data.frame(read.csv(filepath2+'SR_Species_distribution_REVISED_FNN.csv'))
 
 
 SpeciesMap = function(data1, startcolumn){
@@ -140,17 +113,14 @@ SpeciesMap = function(data1, startcolumn){
     # Convert Latitude and Longitude to Numeric variables
     #transform(dataSp, Longitude = as.numeric(Longitude),
     #         Latitude = as.numeric(Latitude))
+    
     dataSp[, 1:3] <- sapply(dataSp[, 1:3], as.numeric)
-    #str(dataSp)
-    #dataSp <- data.frame(read.csv(Name, ".csv"))
     
     m = leaflet(dataSp) %>%
       addProviderTiles(providers$Esri.WorldImagery ) %>%
       addPolygons(data = psbrsShape, color = "#000000", weight = 2, opacity = 1, fillOpacity = 0)%>%
       addCircleMarkers(data = dataSp, lat = ~Latitude, lng = ~Longitude, color = "#FF0000 ", fill = T, fillColor ="#FF0000 ", fillOpacity = 1,  radius = ~2)%>%
       addScaleBar(position = "bottomright", options = scaleBarOptions(maxWidth = 200, metric = T))
-    
-    #if (!dir.exists(SpeciesMaps)) dir.create(SpeciesMaps, recursive = TRUE)
     
     # Save the Species Distribution Map in both PNG and HTML (Interactive) formats
     mapshot(m, file = paste0(getwd(), "_", Name,".png"), url = paste0(getwd(), "/", Name,".html"),
